9201962d8731e770f03b1b4c6d91786c17832244
Steven Lemon <twynsicle@gmai.com>
2022-06-05 16:39:56 +1200

    create tools package to share eslint configuration

--- apps/app1/src/react-app-env.d.ts
+++ apps/app1/src/react-app-env.d.ts
@@ -1,7 +1,3 @@
-/// <reference types="node" />
-/// <reference types="react" />
-/// <reference types="react-dom" />
-
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
@@ -47,7 +43,9 @@ declare module '*.webp' {
 declare module '*.svg' {
   import * as React from 'react';
 
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
 
   const src: string;
   export default src;
