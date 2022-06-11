88778b12f9b6f0b25231a862fe1f1a8126f85c12
Steven Lemon <twynsicle@gmai.com>
2022-06-12 09:38:47 +1200

    add library2

---
+++ libraries/library2/src/Button.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+import buttonStyles from './button.styles';
+
+export interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+const useStyles = createUseStyles(buttonStyles);
+
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const classes = useStyles();
+  const mode = primary ? classes.primary : classes.secondary;
+
+  return (
+    <button
+      type="button"
+      className={[classes.button, classes[size], mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
